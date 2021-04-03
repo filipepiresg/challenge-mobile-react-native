4b44d3424530a08f9f6b0c9eed7d7f89a4ce8cc3
Filipe Pires <filipepiresg@gmail.com>
2021-04-03 14:05:00 -0300

    fix: remove modal unnecessary

--- src/routes/stacks/main.tsx
+++ src/routes/stacks/main.tsx
@@ -1,50 +1,19 @@
 import * as React from 'react';
-import Modal from 'react-native-modal';
-import {useDispatch, useSelector} from 'react-redux';
 
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {ModalContent} from '../../components';
 import {Splash} from '../../pages';
-import {hideCharacter} from '../../store/modules/characters/actions';
-import {CharacterStateInterface} from '../../store/modules/characters/reducer';
-import {Metrics} from '../../styles';
 import TabStack from './tab';
 
 const Stack = createStackNavigator();
 
-export default () => {
-  const dispatch = useDispatch();
-  const {selected} = useSelector(
-    (state: {characters: CharacterStateInterface}) => state.characters,
-  );
-
-  return (
-    <>
-      <Stack.Navigator
-        initialRouteName="Splash"
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="Main" component={TabStack} />
-      </Stack.Navigator>
-
-      <Modal
-        isVisible={!!selected}
-        avoidKeyboard
-        deviceWidth={Metrics.deviceWidth}
-        deviceHeight={Metrics.deviceHeight}
-        animationIn="zoomIn"
-        animationOut="zoomOut"
-        onBackButtonPress={() => {
-          dispatch(hideCharacter());
-        }}
-        onBackdropPress={() => {
-          dispatch(hideCharacter());
-        }}>
-        <ModalContent />
-      </Modal>
-    </>
-  );
-};
+export default () => (
+  <Stack.Navigator
+    initialRouteName="Splash"
+    screenOptions={{
+      headerShown: false,
+    }}>
+    <Stack.Screen name="Splash" component={Splash} />
+    <Stack.Screen name="Main" component={TabStack} />
+  </Stack.Navigator>
+);
