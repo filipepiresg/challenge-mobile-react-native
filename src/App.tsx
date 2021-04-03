6d1cb0518cf1a787d2dda5f673d45a05383b1b2a
Filipe Pires <filipepiresg@gmail.com>
2021-04-03 14:04:16 -0300

    fix: status bar color

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Platform, StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 
 import {PersistGate} from 'redux-persist/integration/react';
@@ -9,6 +10,10 @@ import {store, persistor} from './store/index';
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <StatusBar
+        barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+        backgroundColor="#000"
+      />
       <Routes />
     </PersistGate>
   </Provider>
